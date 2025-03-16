c1970eb7ebb37fc53fbf2fdd99c8fb9e2fd480d9
Hamad Ahmad Ansari <hamad.ansarif90@gmail.com>
2025-03-16 13:54:24 +0530

    Add Convex Database

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,19 @@
-import { Stack } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
-import { tokenCache } from "@/cache";
 import InitialLayout from "@/components/InitialLayout";
+import ClerkAndConvexProvider from "@/provider/ClerkAndConvexProvider";
 
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
 
-if (!publishableKey) {
-  throw new Error(
-    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
-  )
-}
 
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
-      <ClerkLoaded>
+      <ClerkAndConvexProvider>
         <SafeAreaProvider>
           <SafeAreaView style={{ flex: 1, backgroundColor: "#000000"}}>
             <InitialLayout/>
           </SafeAreaView>
         </SafeAreaProvider>
-      </ClerkLoaded>
-    </ClerkProvider>
+      </ClerkAndConvexProvider>
+      
     
   );
 }
