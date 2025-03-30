7502ebfceb6e3e5406cfbad5c94715085076f70f
Hamad Ahmad Ansari <hamad.ansarif90@gmail.com>
2025-03-31 00:13:57 +0530

    feat(User Profile): Add user Profile Page with Profile Edit functionality

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,211 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, ScrollView, FlatList, Modal, TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView, Platform, TextInput } from 'react-native'
+import React, { useState } from 'react'
 import { Link } from 'expo-router'
+import { useAuth } from '@clerk/clerk-expo'
+import { useMutation, useQuery } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+import { Doc } from '@/convex/_generated/dataModel';
+import { Loader } from '@/components/Loader';
+import { styles } from '@/styles/profile.styles';
+import { Ionicons } from '@expo/vector-icons';
+import { COLORS } from '@/constants/theme';
+import { Image } from 'expo-image';
+
 export default function Profile() {
+
+  const { signOut, userId } = useAuth();
+  const [editModal, setEditModal] = useState(false);
+  const currentUser = useQuery(api.users.getUserByClerkId, userId ? { clerkId: userId } : "skip");
+  const [editedProfile, setEditedProfile] = useState({
+    fullname: currentUser?.fullname || "",
+    bio: currentUser?.bio || "",
+  });
+
+  const [selectedPost,setSelectedPost] = useState<Doc<"posts"> | null>(null);
+  const posts = useQuery(api.posts.getPostByUser, {});
+
+  const updateProfile = useMutation(api.users.updateProfile);
+
+  const handleSaveProfile = async () => {
+    await updateProfile(editedProfile);
+    setEditModal(false);
+  };
+
+  if(!currentUser || posts === undefined) return <Loader/>;
+
+
+
   return (
-    <View>
-      <Text>Profile screen</Text>
-      <Link href={"/notifications"}>Go to Notifications Page</Link>
+    <View style={styles.container}>
+     
+      {/* Header */}
+      <View style={styles.header}>
+        <View style={styles.headerLeft}>
+          <Text style={styles.username}>{currentUser.username}</Text>
+        </View>
+        <View style={styles.headerRight}>
+          <TouchableOpacity style={styles.headerIcon} onPress={()=> signOut()}>
+            <Ionicons name='log-out-outline' size={24} color={COLORS.white}/>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.profileInfo}>
+
+          {/* Avatar and Profile Stats */}
+          <View style={styles.avatarAndStats}>
+            <View style={styles.avatarContainer}>
+              <Image 
+                source={currentUser.image}
+                style={styles.avatar}
+                contentFit='cover'
+                transition={200}
+              />
+            </View>
+
+            <View style={styles.statsContainer}>
+              <View style={styles.statItem}>
+                <Text style={styles.statNumber}>{currentUser.posts}</Text>
+                <Text style={styles.statLabel}>Posts</Text>
+              </View>
+              <View style={styles.statItem}>
+                <Text style={styles.statNumber}>{currentUser.followers}</Text>
+                <Text style={styles.statLabel}>Followers</Text>
+              </View>
+              <View style={styles.statItem}>
+                <Text style={styles.statNumber}>{currentUser.following}</Text>
+                <Text style={styles.statLabel}>Following</Text>
+              </View>
+            </View>
+          </View>
+          <Text style={styles.name}>{currentUser.fullname}</Text>
+          {currentUser.bio && <Text style={styles.bio}>{currentUser.bio}</Text>}
+
+          <View style={styles.actionButtons}>
+            <TouchableOpacity style={styles.editButton} onPress={()=> setEditModal(true)}>
+              <Text style={styles.editButtonText}>Edit Profile</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.shareButton}>
+              <Ionicons name='share-outline' size={20} color={COLORS.white}/>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        {posts.length === 0 && <NoPostsFound/>}
+
+        <FlatList
+          data={posts}
+          numColumns={3}
+          scrollEnabled={false}
+          renderItem={({item}) => (
+            <TouchableOpacity style={styles.gridItem} onPress={() => setSelectedPost(item)}>
+              <Image
+                source={item.imageUrl}
+                style={styles.gridImage}
+                contentFit='cover'
+                transition={200}
+              />
+            </TouchableOpacity>
+          )}
+        />
+
+      </ScrollView>
+
+      {/* Edit Profile */}
+      
+      <Modal
+        visible={editModal}
+        animationType='slide'
+        transparent={true}
+        onRequestClose={() => setEditModal(false)}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? "padding" : "height"}
+            style={styles.modalContainer}
+          >
+            <View style={styles.modalContent}>
+              <View style={styles.modalHeader}>
+                <Text style={styles.modalTitle}> Edit Profile</Text>
+                <TouchableOpacity onPress={() => setEditModal(false)}>
+                  <Ionicons name='close' size={24} color={COLORS.white}/>
+                </TouchableOpacity>
+              </View>
+
+              <View style={styles.inputContainer}>
+                <Text style={styles.inputLabel}>Name</Text>
+                <TextInput
+                  style={styles.input}
+                  value={editedProfile.fullname}
+                  onChangeText={(text) => setEditedProfile((prev) => ({...prev, fullname: text}))}
+                  placeholderTextColor={COLORS.grey}
+                />
+              </View>
+
+              <View style={styles.inputContainer}>
+                <Text style={styles.inputLabel}>Bio</Text>
+                <TextInput
+                  style={[styles.input, styles.bioInput]}
+                  value={editedProfile.bio}
+                  onChangeText={(text) => setEditedProfile((prev) => ({...prev, bio: text}))}
+                  multiline
+                  numberOfLines={4}
+                  placeholderTextColor={COLORS.grey}
+                />
+              </View>
+
+              <TouchableOpacity style={styles.saveButton} onPress={handleSaveProfile}>
+                <Text style={styles.saveButtonText}>Save Changes</Text>
+              </TouchableOpacity>
+            </View>
+          </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
+      </Modal>
+
+      {/* Edit Image */}
+      <Modal
+        visible={!!selectedPost}
+        animationType='fade'
+        transparent={true}
+        onRequestClose={() => setSelectedPost(null)}
+      >
+        <View style={styles.modalBackdrop}>
+          {selectedPost && (
+            <View style={styles.postDetailContainer}>
+            <View style={styles.postDetailHeader}>
+              <TouchableOpacity onPress={() => setSelectedPost(null)}>
+                <Ionicons name='close' size={24} color={COLORS.white} />
+              </TouchableOpacity>
+            </View>
+
+            <Image
+              source={selectedPost?.imageUrl}
+              cachePolicy={"memory-disk"}
+              style={styles.postDetailImage}
+            />
+          </View>
+          )}
+        </View>
+      </Modal>
 
     </View>
   )
+}
+
+function NoPostsFound() {
+  return (
+    <View
+      style={{
+        paddingTop: 60,
+        height: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: COLORS.background,
+      }}
+    >
+      <Ionicons name='images-outline' size={48} color={COLORS.primary}/>
+      <Text style={{fontSize: 20, color: COLORS.white}}>No posts yet</Text>
+    </View>
+  );
 }
--- convex/posts.ts
+++ convex/posts.ts
@@ -203,4 +203,24 @@ export const deletePost = mutation({
       });
   
     },
-})
+});
+
+
+export const getPostByUser = query({
+  args: {
+    userId: v.optional(v.id("users")),
+  },
+  handler: async (ctx, args) => {
+    const user = args.userId ? await ctx.db.get(args.userId) : await getAuthenticatedUser(ctx);
+
+    if(!user) throw new Error("User not found");
+    
+    const posts = await ctx.db
+     .query("posts")
+     .withIndex("by_user", (q)=> q.eq("userId", args.userId || user._id))
+     .order("desc")
+     .collect();
+
+    return posts;
+  },
+});
